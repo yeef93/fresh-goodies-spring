ff88d30f33967605c22f62ce64b676efb3fbe1a7
yeef93 <yunindafaranika@gmail.com>
2024-05-29 10:33:20 +0700

    initialcommit

---
+++ src/main/java/com/yeef93/fresh_goodies_spring/FreshGoodiesSpringApplication.java
@@ -0,0 +1,13 @@
+package com.yeef93.fresh_goodies_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshGoodiesSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshGoodiesSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeef93/fresh_goodies_spring/FreshGoodiesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeef93.fresh_goodies_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshGoodiesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
